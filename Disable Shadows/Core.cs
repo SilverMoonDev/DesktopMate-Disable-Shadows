921267b4688667f8505e5686274dcdd9bf774f74
SilverMoonDev <silvermoondev.contact@gmail.com>
2025-01-14 06:18:08 +0100

    Add project files.

---
+++ Disable Shadows/Core.cs
@@ -0,0 +1,48 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(Disable_Shadows.Core), "Disable Shadows", "1.0.0", "Joanpixer", null)]
+[assembly: MelonGame("infiniteloop", "DesktopMate")]
+
+namespace Disable_Shadows
+{
+    public class Core : MelonMod
+    {
+        private int characterID;
+        private Transform rootTransform;
+
+        public override void OnLateUpdate()
+        {
+            if (rootTransform == null || rootTransform.childCount == 0) return;
+
+            var newCharacter = rootTransform.GetChild(0).gameObject;
+            if (newCharacter.GetInstanceID() != characterID)
+            {
+                OnAvatarChange(newCharacter);
+            }
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "Main" && rootTransform == null)
+            {
+                rootTransform = GameObject.Find("CharactersRoot")?.transform;
+            }
+        }
+
+        private void OnAvatarChange(GameObject avatar)
+        {
+            if (avatar == null) return;
+            characterID = avatar.GetInstanceID();
+            DisableShadows(avatar);
+        }
+
+        private void DisableShadows(GameObject avatar)
+        {
+            var meshRenderers = avatar.GetComponentsInChildren<SkinnedMeshRenderer>();
+            foreach (SkinnedMeshRenderer meshRenderer in meshRenderers)
+            {
+                meshRenderer.castShadows = false;
+            }
+        }
+    }
+}
